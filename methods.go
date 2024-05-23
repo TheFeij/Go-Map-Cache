13e655c6cbe3fe32a2e0eaf2ce4d2b598bd9a976
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-23 11:35:41 +0330

    defined CacheData method to save data to cache

--- methods.go
+++ methods.go
@@ -1,5 +1,45 @@
 package mapcache
 
+import (
+	"fmt"
+	"time"
+)
+
+// CacheData saves the input key and value to cache
+func (c *MapCache) CacheData(key string, value interface{}, duration time.Duration) (err error) {
+	// capture panics
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			err = fmt.Errorf("%w: %v", ErrCacheUnsuccessful, r)
+		}
+	}()
+
+	// create the new entry
+	newEntry := newCacheEntry(key, value, duration)
+
+	// acquire read lock and then release it at the end of the function
+	c.rwMutex.Lock()
+	defer c.rwMutex.Unlock()
+
+	// Check if the entry size exceeds the max entry size
+	if newEntry.size > c.maxEntrySize {
+		err = ErrEntrySizeLimit
+		return
+	}
+
+	// Check if the new entry exceeds the memory limit
+	if c.curMemory+newEntry.size > c.maxMemory {
+		err = ErrMemoryLimit
+		return
+	}
+
+	// store key and value in the cache
+	c.storage[key] = newEntry
+
+	return
+}
+
 // deleteEntry Removes the entry from cache
 func (c *MapCache) deleteEntry(entry *mapCacheEntry) {
 	delete(c.storage, entry.key)
