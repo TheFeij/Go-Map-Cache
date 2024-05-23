5ad2e7aa2451c37ee687a9749fc48725ece5ad9b
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-23 11:36:01 +0330

    defined LoadData method to read data from cache

--- methods.go
+++ methods.go
@@ -40,6 +40,22 @@ func (c *MapCache) CacheData(key string, value interface{}, duration time.Durati
 	return
 }
 
+// LoadData loads the data relative to the input key
+// returns an error object if key does not exist in the cache
+func (c *MapCache) LoadData(key string) (value interface{}, err error) {
+	// acquire read lock and then release it at the end of the function
+	c.rwMutex.RLock()
+	defer c.rwMutex.RUnlock()
+
+	cacheEntry, found := c.storage[key]
+	if !found || cacheEntry.isExpired() {
+		err = ErrKeyNotFound
+		return
+	}
+
+	return cacheEntry.value, nil
+}
+
 // deleteEntry Removes the entry from cache
 func (c *MapCache) deleteEntry(entry *mapCacheEntry) {
 	delete(c.storage, entry.key)
